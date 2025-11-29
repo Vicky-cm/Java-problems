b237fd46b655a5aace588ba1298e09d1bea49efe
Vicky-cm <balavignesh742@gmail.com>
2025-11-29 16:27:43 +0530

    Create  Practice.java

---
+++ Practice.java
@@ -0,0 +1,29 @@
+package Dailypractice;
+
+
+public class Practice {
+
+    public static void main (String[] abcd)
+    {
+        byte age = 37;
+        short net_amount = 30000;
+        int price = 50000000;
+        long turnover = 30000000;
+        float deci = 4.627762737f;
+        double dou = 432.8657287d;
+        char  che = 's';
+        boolean boo = true;
+
+       System.out.println("Hello User");
+        System.out.println("Your age is:"+age);
+        System.out.println("Your net amount is:"+net_amount);
+        System.out.println("Your price is:"+price);
+        System.out.println("Your turnover is:"+turnover);
+        System.out.println("Your net profit is:"+deci);
+        System.out.println("Your double amount is:"+dou);
+        System.out.println("Your character is:"+che);
+        System.out.println("Your value is:"+boo);
+
+    }
+
+}
